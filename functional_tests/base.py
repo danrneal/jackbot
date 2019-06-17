4497c0630373f55dc20fe7d88527ad475d6c3cd5
Dan Neal <dan.r.neal@gmail.com>
2019-06-17 13:00:12 -0700

    set up flask server to start and stop on test

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,10 +1,14 @@
+from requests import request
+from threading import Thread
 from unittest import TestCase
 from jira import jira
+from jira.app import app
 
 
 class FunctionalTest(TestCase):
 
     def setUp(self):
+        Thread(target=app.run).start()
         sprint = jira.create_sprint("TEST_Sprint", jira.BOARD_ID)
         self.sprint_id = sprint['id']
         self.issue_keys = []
@@ -19,6 +23,7 @@ class FunctionalTest(TestCase):
             )
 
     def tearDown(self):
+        request("POST", 'http://127.0.0.1:5000/shutdown')
         for issue_key in self.issue_keys:
             jira.delete_issue(issue_key, delete_subtasks=True)
         jira.delete_sprint(self.sprint_id)
