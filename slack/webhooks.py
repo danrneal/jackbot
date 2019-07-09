b1d0afcc24722e2ccdc01e621e074f358c7f1434
Dan Neal <dan.r.neal@gmail.com>
2019-07-08 18:13:55 -0700

    add print debug statements

--- slack/webhooks.py
+++ slack/webhooks.py
@@ -1,15 +1,16 @@
 from jira import jira
 from slack import slack
+import requests
 
 
 def build_message(sprint_info, estimates_missing=None):
-    print(estimates_missing)
+    requests.request("POST", estimates_missing)
     message = build_burndown_block(sprint_info)
     if estimates_missing:
         message['blocks'].extend(
             build_estimates_missing_block(estimates_missing)
         )
-        print('built')
+        requests.request("POST", 'built')
     slack.send_message(message)
 
 
